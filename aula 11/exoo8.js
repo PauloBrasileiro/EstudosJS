1364f4d2aae51cc93bad7d4b647c4d37b0085c3c
Paulo Brasileiro <pauloh.b@hotmail.com>
2022-03-14 15:23:35 -0300

    Estudos js

---
+++ aula 11/exoo8.js
@@ -0,0 +1,6 @@
+var vel = 40.5
+    console.log(`A velocidade do seu carro é ${vel}`)
+if (vel>60){//condição simples
+    console.log("Passou o limite de velocidade")
+}
+    console.log("Se beber, não dirija")
