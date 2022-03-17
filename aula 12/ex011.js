df35b7cc9e806ceb796a1672f577a62f51e99c04
Paulo Brasileiro <pauloh.b@hotmail.com>
2022-03-17 13:24:42 -0300

    Ex011 idade

---
+++ aula 12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 70
+if (idade >= 18){
+    console.log("Voto obrigatório")
+}else if(idade >= 16 || idade >= 65){
+console.log("Voto opcional")
+}
+else {
+    console.log("Não vota")
+}
