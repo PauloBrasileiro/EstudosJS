3997430ee959269377e4896c58b4a7d41c359eb2
Paulo Brasileiro <pauloh.b@hotmail.com>
2022-03-17 13:42:49 -0300

    ex012 alteração

---
+++ aula 12/ex012.js
@@ -0,0 +1,18 @@
+var agora = new Date
+var hora = agora.getHours()
+// Ele pega tanto horas, como datas atual
+console.log(`São ${hora} horas`)
+if(hora < 12 && hora > 4){
+    console.log("Bom dia")
+}else if(hora == 12){
+    console.log("Bom almoço")
+}
+else if(hora > 12 && hora <= 17){
+    console.log("Boa Tarde")
+}
+else if (hora >= 18 && hora <= 23){
+    console.log("Boa noite")
+}
+else{
+    console.log("Madrugada")
+}
